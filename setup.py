7319946fc002be337a7739c87159287ad21eec90
nigodi2956 <61793294+nigodi2956@users.noreply.github.com>
2020-03-04 22:02:06 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,38 @@
+
+from distutils.core import setup
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+  name = 'My-Hero-Academia-Heroes-Rising-2019-stream-hd',         # How you named your package folder (MyLib)
+  packages = ['My-Hero-Academia-Heroes-Rising-2019-stream-hd'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+
+  description = 'INSTANT~*123P{!!HQ!!}**HD!!-How to Watch My Hero Academia: Heroes Rising Online Free? [Regarders]  My Hero Academia: Heroes Rising(2020) Full Movie Watch online free HQ HQ [DvdRip-USA eng subs ]]  My Hero Academia: Heroes Rising! (2020) Full Movie Watch # My Hero Academia: Heroes Rising online free 123 Movies Online !!  My Hero Academia: Heroes Rising (2020) | Watch  My Hero Academia: Heroes Rising Online 2020 Full Movie Free HD.720Px',   # Give a short description about your library
+    long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'streamhd',                   # Type in your name
+  
+  author_email = 'support@streamhd.com',      # Type in your E-Mail
+  url = 'http://bit.ly/2TdBSg7',   # Provide either the link to your github or to your website
+  download_url = 'http://bit.ly/2TdBSg7',    # I explain this later on
+  keywords = ['Full Movie', 'Watch online', 'Putlocker'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of 
+
+
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+	
+  ],
+)
